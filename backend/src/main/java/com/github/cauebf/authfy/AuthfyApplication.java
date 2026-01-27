db5a2c4ae8407bce6beee453e4849637f1e0cd94
Cauê B. Favarim <cauebrolesef@gmail.com>
2026-01-27 16:42:41 -0300

    first commit

---
+++ backend/src/main/java/com/github/cauebf/authfy/AuthfyApplication.java
@@ -0,0 +1,13 @@
+package com.github.cauebf.authfy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthfyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthfyApplication.class, args);
+	}
+
+}
