92ded0ddae84d000db38966d78bc641c103fd557
Cauê B. Favarim <cauebrolesef@gmail.com>
2026-01-27 19:25:19 -0300

    feat: add ProfileController with registration endpoint

---
+++ backend/src/main/java/com/github/cauebf/authfy/controller/ProfileController.java
@@ -0,0 +1,29 @@
+package com.github.cauebf.authfy.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+import com.github.cauebf.authfy.io.ProfileRequest;
+import com.github.cauebf.authfy.io.ProfileResponse;
+import com.github.cauebf.authfy.service.ProfileService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class ProfileController {
+
+    private final ProfileService profileService;
+    
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProfileResponse register(@Valid @RequestBody ProfileRequest request) {
+        ProfileResponse response = profileService.createProfile(request);
+        // TODO: send welcome email
+        return response;
+    }
+}
